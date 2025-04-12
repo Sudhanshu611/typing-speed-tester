02347056a27131155d3142c1be34988eddd00db2
Sudhanshu Singh <sudhanshu0300@gmail.com>
2025-04-12 08:06:44 +0530

    Added Pass/Fail indicator on letters

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
+import { sentence } from './sentence.js';
+
 // Display Elements
 const timerHTML = document.querySelector('.timer');
+
+ const displayUnits = {
+    wpm: 0,
+    cpm: 0,
+    accuracy:0,
+    mistakes:0
+}
+
 const wpmHTML = document.querySelector('.wpm');
 const cpmHTML = document.querySelector('.cpm');
 const accuracyHTML = document.querySelector('.accuracy');
 const mistakesHTML = document.querySelector('.mistakes');
 
 // Quote Area
-const quoteArea = document.querySelector('.js-quote-area');
+const sentenceArea = document.querySelector('.js-quote-area');
 
 // Text Area
 const textInput = document.querySelector('.js-text-area');
 
+let arr = sentence.split('').map((letter) => {
+    return `<span class="sentence-chars">${letter}</span>`
+});
 
-// https://random-word-api.herokuapp.com/word
-let quoteWord = [];
-let quoteSentence = []
-async function quoteGenerator(){
+sentenceArea.innerHTML = arr.join('');
 
-    const response = await fetch('https://random-word-api.herokuapp.com/word');
-    const data = await response.json();
-    return data[0];
-}
-
-async function getQuote() {
-    const promises = [];
-    for (let i = 0; i < 20; i++){
-        promises.push(quoteGenerator());
-    }
-
-    quoteWord = await Promise.all(promises);
-    quoteSentence = quoteWord.join(' ');
-    quoteWord = quoteSentence.split('').map((letter) => {
-        return '<span class="">' + letter + '</span>'
+textInput.addEventListener('input', () => {
+    let userInput = textInput.value.split('');
+    document.querySelectorAll('.sentence-chars').forEach((letter, index) =>{
+        if (letter.innerText === userInput[index]){
+            letter.classList.add('pass');
+        }
+        else if(userInput[index] == null){
+            if (letter.classList.contains('pass')){
+                letter.classList.remove('pass');
+            }
+            else if(letter.classList.contains('fail')){
+                letter.classList.remove('fail');
+            }
+        }
+        // not letter.innerText !== userInput[index] because its true for every letter in sentence that has not been typed
+        else if (!letter.classList.contains('fail')){
+            letter.classList.add('fail');
+        }
     });
-    quoteSentence = quoteWord.join('');
-    console.log(quoteWord);
-    quoteArea.innerHTML = quoteSentence;
-}
-
-
-getQuote();
+});
---
+++ sentence.js
@@ -0,0 +1,30 @@
+const simpleWords = [
+    // Actions (Verbs)
+    "run", "walk", "jump", "play", "eat", "go", "sit", "stand", "write", "read",
+    "talk", "sleep", "cook", "draw", "smile", "think", "cry", "drink", "swim", "ride",
+  
+    // Prepositions
+    "in", "on", "under", "over", "between", "beside", "with", "without", "by", "near",
+    "from", "to", "into", "onto", "around", "through",
+  
+    // Nouns
+    "dog", "cat", "ball", "book", "pen", "car", "tree", "boy", "girl", "house",
+    "chair", "table", "door", "road", "water", "food", "school", "sun", "moon", "bed",
+  
+    // Adjectives
+    "big", "small", "fast", "slow", "happy", "sad", "hot", "cold", "soft", "hard",
+  
+    // Conjunctions / Fillers
+    "and", "but", "or", "so", "then", "yet", "when", "while", "if", "because"
+  ];
+
+
+function sentenceGenerator(){
+    let lst = []
+    for (let i = 0; i < 20; i++){
+        const randomNumber = Math.floor(Math.random() * simpleWords.length) + 1;
+        lst.push(simpleWords[randomNumber]);
+    }
+    return lst;
+}
+export const sentence = (sentenceGenerator()).join(' ');
